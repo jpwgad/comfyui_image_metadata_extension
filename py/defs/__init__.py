22f146bf82d9524ff8e84dc059f2021097f2779e
edelvarden <edelvarden@gmail.com>
2025-04-17 21:09:53 +0300

    fix: #18

--- py/defs/__init__.py
+++ py/defs/__init__.py
@@ -1,6 +1,7 @@
 import glob
 import importlib
 import os
+import sys
 
 from .captures import CAPTURE_FIELD_LIST
 from .samplers import SAMPLERS
@@ -8,11 +9,28 @@ from .samplers import SAMPLERS
 # load CAPTURE_FIELD_LIST and SAMPLERS in ext folder
 dir_name = os.path.dirname(os.path.abspath(__file__))
 ext_folder = os.path.join(dir_name, "ext")
+
+# Add the base directory (py/defs) to sys.path to ensure correct module imports
+sys.path.append(os.path.join(dir_name, 'defs'))
+
 parent_folder = os.path.basename(os.path.dirname(os.path.dirname(dir_name)))
 
 for module_path in glob.glob(os.path.join(ext_folder, "*.py")):
     module_name = os.path.splitext(os.path.basename(module_path))[0]
     package_name = f"custom_nodes.{parent_folder}.py.defs.ext.{module_name}"
-    module = importlib.import_module(package_name)
-    CAPTURE_FIELD_LIST.update(getattr(module, "CAPTURE_FIELD_LIST", {}))
-    SAMPLERS.update(getattr(module, "SAMPLERS", {}))
+
+    try:
+        # Import the module dynamically
+        module = importlib.import_module(package_name)
+        
+        if hasattr(module, "CAPTURE_FIELD_LIST"):
+            CAPTURE_FIELD_LIST.update(getattr(module, "CAPTURE_FIELD_LIST", {}))
+        
+        if hasattr(module, "SAMPLERS"):
+            SAMPLERS.update(getattr(module, "SAMPLERS", {}))
+    except Exception as e:
+        print(f"Failed to import {module_name}: {e}")
+
+# Clean up sys.path after import
+if os.path.join(dir_name, 'defs') in sys.path:
+    sys.path.remove(os.path.join(dir_name, 'defs'))
